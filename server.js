ccd90da76132a420cec8d18fbe3ae77fe4d5d639
Joseph Amador <kirito.1986.714@gmail.com>
2021-07-13 16:31:28 -0700

    add employee, update employee role, view all employees

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const inquirer = require("inquirer");
 
 const mysql = require("mysql");
+const { exit } = require("process");
+const util = require("util");
 
 const allController = require("./controllers/all");
+let connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Be sure to update with your own MySQL password!
+  password: "",
+  database: "employee_TrackerDB",
+});
+connection.connect();
+connection.query = util.promisify(connection.query);
+
 // // import controllers
 // const departmentController = require("./controllers/department");
 
@@ -17,11 +35,12 @@ const mainMenu = async () => {
         choices: [
           "view all employees",
           "view all employees by role",
-          "view all employees by department",
+          "view all departments",
           "add employee",
           "add department",
           "add role",
           "update employee role",
+          "Exit",
         ],
       },
     ])
@@ -34,7 +53,8 @@ const mainMenu = async () => {
         case "view all employees by role":
           displayAllByRole(mainMenu);
           break;
-        case "view all employees by department":
+        case "view all departments":
+          displayAllDept();
           break;
         case "add employee":
           addEmployee();
@@ -44,7 +64,10 @@ const mainMenu = async () => {
         case "add role":
           break;
         case "update employee role":
+          updateEmployee();
           break;
+        case "Exit":
+          process.exit();
       }
     });
 };
@@ -75,20 +98,8 @@ const displayAll = (callBack = () => {}) => {
 
   connection.end();
 };
+
 const displayAllByRole = (callBack = () => {}) => {
-  let connection = mysql.createConnection({
-    host: "localhost",
-
-    // Your port; if not 3306
-    port: 3306,
-
-    // Your username
-    user: "root",
-
-    // Be sure to update with your own MySQL password!
-    password: "",
-    database: "employee_TrackerDB",
-  });
   let roleArray = [];
   let query1 = `SELECT title FROM role;`;
   connection.query(query1, function (err, res) {
@@ -113,101 +124,184 @@ const displayAllByRole = (callBack = () => {}) => {
       });
   });
 };
+async function updateEmployee() {
+  let checkEmployees = await connection.query(
+    `SELECT employee.id, concat(employee.first_name, ' ', employee.last_name) AS Employee FROM employee ORDER BY Employee ASC;`
+  );
+  let allRoles = await connection.query(
+    `SELECT id, title FROM role ORDER BY title ASC; `
+  );
+  checkEmployees = checkEmployees.map((i) => ({
+    name: i.Employee,
+    value: i.id,
+  }));
+
+  allRoles = allRoles.map((i) => ({ name: i.title, value: i.id }));
+
+  const {roleId, employeeId } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "employeeId",
+      message: "which employee do you want to update?",
+      choices: checkEmployees,
+    },
+    {
+      type:"list",
+      name:"roleId",
+      message:"whats their new role?",
+      choices: allRoles,
+    }
+  ]);
+  connection.query("UPDATE employee SET role_id = ? WHERE id = ?",[roleId, employeeId]);
+  mainMenu();
+}
+
+async function displayAllDept() {
+  let allDept = await connection.query(`SELECT id, name FROM department`);
+  // allDept.map((dept) => dept.name).forEach((name) => console.log(name));
+  console.table(allDept);
+
+  mainMenu();
+}
 
 async function addEmployee() {
-  console.log(`Adding employee`);
-  let connection = mysql.createConnection({
-    host: "localhost",
+  let allRoles = await connection.query(
+    `SELECT id, title FROM role ORDER BY title ASC; `
+  );
+  const allEmployees = await connection.query(
+    `SELECT employee.id, concat(employee.first_name, ' ', employee.last_name) AS Employee FROM employee ORDER BY Employee ASC;`
+  );
+  allRoles = allRoles.map((i) => ({ name: i.title, value: i.id }));
+  //  allEmployees = allEmployees.map(i=> ({name:i.title, value:i.id}));
+  console.log(allRoles, allEmployees);
 
-    // Your port; if not 3306
-    port: 3306,
-
-    // Your username
-    user: "root",
-
-    // Be sure to update with your own MySQL password!
-    password: "",
-    database: "employee_TrackerDB",
-  });
-  let roleArray = [];
-  let managerArray = [];
-  //
-  const allResults = await Promise.all([
-    // query selecting ID,title from role
-    connection.query(`SELECT id, title FROM role ORDER BY title ASC; `,([rows, fields]) => {
-      console.log(rows);
-        return rows;
-      }),
-      // query selecting by employee.id and concat first and last name AS Employee from the employee table
-    connection.query(`SELECT employee.id, concat(employee.first_name, ' ', employee.last_name) AS Employee FROM employee ORDER BY Employee ASC;`
-      ,(([rows, fields]) => {
-        return rows;
-      }),
-  )]);
-  // allResults is a array with of arrays for 0
-  for (let i = 0; i < allResults[0].length; i++) {
-    roleArray.push(allResults[0][i].title);
-  }
-   // allResults is a array with of arrays for 1
-  for (let i = 0; i < allResults[1].length; i++) {
-    managerArray.push(allResults[1][i].Employee);
-  }
-  console.log(roleArray);
-  console.log(managerArray);
-  // prompt to add a employee
-  await inquirer
-    .prompt([
-      {
-        name: "first_name",
-        type:`input`,
-        message: "add your employee first name",
-        validate:(input)=>{
-          if(input === ``){
-            console.log(`Enter a name`);
-            return false;
-          }
-          return true;
+  const response = await inquirer.prompt([
+    {
+      name: "first_name",
+      message: "add your employee first name",
+      validate: (input) => {
+        if (input === ``) {
+          console.log(`Enter a name`);
+          return false;
         }
+        return true;
       },
-      {
-        name: "last_name",
-        message: "add your employee last name",
-      },
-      {
-        name: `role`,
-        type: "list",
-        message: `whats the employee role ?`,
-        choices: roleArray,
-      },
-      {
-        name: `manager`,
-        type: "list",
-        message: `Who is the Manager ?`,
-        choices: managerArray,
-      },
-    ])
-    // .then to carry the answer over to insert our user data into connection.query
-    .then((answer) => {
-      let roleId = null;
-      let managerId = null;
-      for (let i = 0; i < allResults[0].length; i++) {
-        roleId = allResults[0][i].id;
-      }
-      for (let i = 0; i < allResults[1].length; i++) {
-        managerId = allResults[1][i].id;
-      }
-      // inserting into employee first and last name role id and manager id using template literals to insert our answers and ids.
-      connection.query(
-        `INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES("${answers.first_name}","${answers.last_name}",${roleId},${managerId});`,
-        (err, res) => {
-          if (err) throw err;
-          console.log(
-            `\n employee ${answer.first_name} ${answer.last_name}  was  added!\n`
-          );
-          console.log(`${res.affectedRows} employee added!\n`);
-          mainMenu();
-          connection.end();
-        }
-      );
-    });
+    },
+    {
+      name: "last_name",
+      message: "add your employee last name",
+    },
+    {
+      name: `role_id`,
+      type: "list",
+      message: `whats the employee role ?`,
+      choices: allRoles,
+    },
+    // {
+    //   name: `manager`,
+    //   type: "list",
+    //   message: `Who is the Manager ?`,
+    //   choices: managerArray,
+    // },
+  ]);
+  console.log(response);
+  connection.query(`INSERT INTO employee SET ?`, response);
+  mainMenu();
 }
+
+// async function oldA() {
+//   console.log(`Adding employee`);
+//   let connection  = await mysql.createConnection({
+//     host: "localhost",
+
+//     // Your port; if not 3306
+//     port: 3306,
+
+//     // Your username
+//     user: "root",
+
+//     // Be sure to update with your own MySQL password!
+//     password: "",
+//     database: "employee_TrackerDB",
+//   });
+//   let roleArray = [];
+
+//   let managerArray = [];
+//   //
+//   const allResults = await Promise.all([
+// //     // query selecting ID,title from role
+//     connection.query(`SELECT id, title FROM role ORDER BY title ASC; `,([rows, fields]) => {
+//       console.log(rows);
+//         return rows;
+//       }),
+//       // query selecting by employee.id and concat first and last name AS Employee from the employee table
+//     connection.query(`SELECT employee.id, concat(employee.first_name, ' ', employee.last_name) AS Employee FROM employee ORDER BY Employee ASC;`
+//       ,(([rows, fields]) => {
+//         return rows;
+//       }),
+//   )]);
+//   // allResults is a array with of arrays for 0
+//   for (let i = 0; i < allResults[0].length; i++) {
+//     roleArray.push(allResults[0][i].title);
+//   }
+//    // allResults is a array with of arrays for 1
+//   for (let i = 0; i < allResults[1].length; i++) {
+//     managerArray.push(allResults[1][i].Employee);
+//   }
+//   console.log(roleArray);
+//   console.log(managerArray);
+//   // prompt to add a employee
+//   await inquirer
+// .prompt([
+//   {
+//     name: "first_name",
+//     message: "add your employee first name",
+//     // validate:(input)=>{
+//     //   if(input === ``){
+//     //     console.log(`Enter a name`);
+//     //     return false;
+//     //   }
+//     //   return true;
+//     // }
+//   },
+//   {
+//     name: "last_name",
+//     message: "add your employee last name",
+//   },
+//   {
+//     name: `role`,
+//     type: "list",
+//     message: `whats the employee role ?`,
+//     choices: roleArray,
+//   },
+//   {
+//     name: `manager`,
+//     type: "list",
+//     message: `Who is the Manager ?`,
+//     choices: managerArray,
+//   },
+// ])
+//     .then((answer) => {
+//       let roleId = null;
+//       let managerId = null;
+//       for (let i = 0; i < allResults[0].length; i++) {
+//         roleId = allResults[0][i].id;
+//       }
+//       for (let i = 0; i < allResults[1].length; i++) {
+//         managerId = allResults[1][i].id;
+//       }
+//       // inserting into employee first and last name role id and manager id using template literals to insert our answers and ids.
+//       connection.query(
+//         `INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES("${answers.first_name}","${answers.last_name}",${roleId},${managerId});`,
+//         (err, res) => {
+//           if (err) throw err;
+//           console.log(
+//             `\n employee ${answer.first_name} ${answer.last_name}  was  added!\n`
+//           );
+//           console.log(`${res.affectedRows} employee added!\n`);
+//           mainMenu();
+//           connection.end();
+//         }
+//       );
+//     });
+// }
